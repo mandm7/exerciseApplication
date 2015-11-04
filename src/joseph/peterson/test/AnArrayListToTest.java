0f9a4664f7b4bf0cf5d3fb8a10908086dd33cb4f
Joseph <japeter89@gmail.com>
2015-11-04 12:31:09 -0700

    JSON, JUNIT

---
+++ src/joseph/peterson/test/AnArrayListToTest.java
@@ -0,0 +1,16 @@
+package joseph.peterson.test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Joseph on 11/3/2015.
+ */
+public class AnArrayListToTest {
+    //create attribute for class that is an arrayList.
+    private ArrayList<Integer> testArray = new ArrayList<>();
+    //create method to append a number to arrayList attribute.
+        public void appendNumber(int aNumber){
+            testArray.add(aNumber);
+        }
+
+}
---
+++ src/joseph/peterson/test/MyJunitTest.java
@@ -0,0 +1,60 @@
+package joseph.peterson.test;
+
+
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by Joseph on 11/2/2015.
+ */
+public class MyJunitTest {
+
+    @Test
+    public void testAddPerson() {
+        int sum = 3 + 3;
+        assertEquals(sum,6);
+
+        double sumOne = 2.4 + 2.7;
+        assertNotEquals(5.1,4);
+
+        //checking two arrays to see if they equal.
+        int[] anArray = {1,2,3,4,5,6,7,8,9,10};
+        int[] anotherArray = {1,2,3,4,5,6,7,8,9,10};
+        assertArrayEquals(anArray, anotherArray);
+
+        //assertNotSame
+        assertNotSame("this","that");
+
+        //asserts an object isn't null.
+        assertNotNull(10);
+        assertNull(null);
+        assertSame(2,2);
+        assertThat("123",is("123"));
+
+        //set attribute of AnArrayListToTest class to public
+
+        //first we need instance
+        AnArrayListToTest ATest = new AnArrayListToTest();
+        ATest.appendNumber(1);
+        //get the attribute and store in variable.
+        try{
+            Field myField = AnArrayListToTest.class.getDeclaredField("testArray");
+            //set variable to public.
+            myField.setAccessible(true);
+            //get what is in the attribute and test it.
+            int aNumber = (Integer)myField.get(ATest);
+            assertEquals(1, aNumber);
+            //change what is in the attribute.
+            aNumber = 2;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
