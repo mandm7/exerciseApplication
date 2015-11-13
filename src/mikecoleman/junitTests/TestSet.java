0660019d196b2a050b941f334b721058a27b98ac
mandm7 <col11024@byui.edu>
2015-11-13 11:20:36 -0700

    almost done with Junit

---
+++ src/mikecoleman/junitTests/TestSet.java
@@ -0,0 +1,18 @@
+package mikecoleman.junitTests;
+
+import java.util.HashSet;
+
+/**
+ * Created by Mike on 11/13/2015.
+ * This is a class that has a private attribute exampleSet of type HashSet
+ * Our Junit test will be to add and change an element within the private HashSet
+ */
+
+public class TestSet {
+    private HashSet<String> exampleSet = new HashSet<>();
+    //create method to append a number to arrayList attribute.
+    public void addString(String someString){
+        exampleSet.add(someString);
+        System.out.println(exampleSet);
+    }
+}
